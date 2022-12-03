0627afbc4f35e92b26ecb548eec106156d272a87
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2022-12-03 10:33:16 +0000

    init

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 8):
-    for j in range(i + 1, 9):
-        print("{:d}{:d}".format(i, j), end=", ")
-print("{:d}{:d}".format(i + 1, j))
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
+        else:
+            print("{:d}{:d}".format(i, j), end=", ")
