903d2159035d7be7d8206ed3a126170286b4f584
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2022-12-05 11:42:52 +0000

    init

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import add from add_0
+
+    a = 1
+    b = 2
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1
+
+    a = 10
+    b = 5
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sud(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
