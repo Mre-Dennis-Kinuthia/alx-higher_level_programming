97b744ff0f5c4e311413321a046b8d063a1abece
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2022-12-07 15:27:58 +0000

    init

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
+
+
 def print_arg(argv):
     n = len(argv) - 1
     if n == 1:
@@ -12,6 +14,7 @@ def print_arg(argv):
         while i <= n:
             print("{:d}: {:s}".format(i, argv[i]))
             i += 1
+
 
 if __name__ == "__main__":
     print_arg(argv)
