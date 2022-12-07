b3ec2c6297b7a3449c336b7c9005b9dc2b3f7d5f
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2022-12-07 18:55:18 +0000

    init

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,11 +6,17 @@ def my_calc():
     if args_count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    a = sys.argv[1]
+    operator = sys.argv[2]
+    b = sys.argv[3]
     elif sys.argv[2] != '-' and sys.argv[2] != '+' and sys.argv[2] != '*' and sys.argv!= '/':
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
-        print("{:d} {:s} = {:d}".format(sys.argv[1], sys.argv[2], sys.argv[3]))
+        print("{:d} {:s} = {:d}".format(a, operator, b, add(a, b)))
+        print("{:d} {:s} = {:d}".format(a, operator, b, sub(a, b)))
+        print("{:d} {:s} = {:d}".format(a, operator, b, mul(a, b)))
+        print("{:d} {:s} = {:d}".format(a, operator, b, div(a, b)))
 
 
 if __name__ == "__main__":
