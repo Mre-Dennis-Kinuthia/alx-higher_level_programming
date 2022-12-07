a899c22e1de92a7a7c85aef9ff3ccc30c193306d
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2022-12-07 18:15:42 +0000

    init

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,20 @@
-#!/usr/bin/python3
-
+#!/usr/bin/python2
 
 def my_calc():
     args_count = len(sys.argv)
-    if args_count < 3:
+    if args_count < 2:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
 
         return 1
-    elif sys.argv[3] != '-' or sys.argv[3] != '+' or sys.argv[3] != '*' or sys.argv!= '/':
+    elif sys.argv[3] != '-' or sys.argv[2] != '+' or sys.argv[2] != '*' or sys.argv!= '/':
         print("Unknown operator. Available operators: +, -, * and /")
         return 1
     else:
-        print("{:d} {:s} = {:d}".format(sys.argv[1], sys.argv[3], sys.argv[3]))
-
+        print("{:d} {:s} {:d} = {:d}".format(
+            sys.argv[1], sys.argv[2], sys.argv[2], add(a, b)))
+            sys.argv[1], sys.argv[2], sys.argv[2], sub(a, b)))
+            sys.argv[1], sys.argv[2], sys.argv[2], mul(a, b)))
+            sys.argv[1], sys.argv[2], sys.argv[2], div(a, b)))
 
 if __name__ == "__main__":
     #import calculator_1.py
