f4f14978515f8a0f9381c9bbfa624b2e18d91651
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2022-12-07 18:59:48 +0000

    init

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 
 
-def my_calc():
-    args_count = len(sys.argv)
-    if args_count != 3:
+def arg_calc(argv):
+    n = len(argv) - 1
+    if n != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a = sys.argv[1]
-    operator = sys.argv[2]
-    b = sys.argv[3]
-    elif sys.argv[2] != '-' and sys.argv[2] != '+' and sys.argv[2] != '*' and sys.argv!= '/':
+    a = int(argv[1])
+    op = argv[2]
+    b = int(argv[3])
+    if op == '+':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
+    elif op == '-':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
+    elif op == '*':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
+    elif op == '/':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, div(a, b)))
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    else:
-        print("{:d} {:s} = {:d}".format(a, operator, b, add(a, b)))
-        print("{:d} {:s} = {:d}".format(a, operator, b, sub(a, b)))
-        print("{:d} {:s} = {:d}".format(a, operator, b, mul(a, b)))
-        print("{:d} {:s} = {:d}".format(a, operator, b, div(a, b)))
 
 
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     import sys
-    my_calc()
+    arg_calc(sys.argv)
