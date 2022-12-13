b46592122c9038fe56ad77f7e3407eeaace43ce4
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2022-12-13 14:37:28 +0000

    initial commit

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,4 +6,4 @@ def replace_in_list(my_list, idx, element):
         for i in range(0, len(my_list)):
             if i == idx:
                 my_list[idx] = element
-                return (my_list[idx])
+                return (my_list)
