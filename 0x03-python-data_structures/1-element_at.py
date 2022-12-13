6c90a8fdbb724bf18d4da2309870314a2d679e67
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2022-12-13 14:28:29 +0000

    initial commit

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
 def element_at(my_list, idx):
-    if idx < 0 and idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
     else:
         for i in range(0, len(my_list)):
-            print("{:d}".format(my_list[i]))
+            if i == idx:
+                return my_list[idx]
