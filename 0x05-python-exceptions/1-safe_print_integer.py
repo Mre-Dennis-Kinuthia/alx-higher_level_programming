c36d05bddb9164bdbd661f9760f1a7497d374d80
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2022-12-21 17:38:49 +0000

    init

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        if value is not None and value is not str:
-            print("{:d}".format(value))
-            return True
+        print("{:d}".format(value))
+        return True
     except:
         return False
