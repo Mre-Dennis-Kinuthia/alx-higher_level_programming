7366abf2fe78081c011f63fcd4e780378b0eb514
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2022-12-21 16:19:05 +0000

    init

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
-            print(my_list[i], end=" ")
-    except IndexError:
-        pass
-    print()
-    return i + 1
+    index = 0
+    while True:
+        try:
+            if index < x:
+                print(my_list[index], end='')
+                index += 1
+            else:
+                print()
+                return index
+        except IndexError:
+            print()
+            return index
+
