c74203599c2be85cfe818ba9cc14c7c6b5f9bd7d
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2022-12-21 14:52:12 +0000

    init

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print(my_list[i])
+    except NameError:
+        print("Error: index out of range")
+        
