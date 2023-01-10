830eb753f7dbc17763771ec4836fde703320bd14
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-01-10 14:44:41 +0000

    init

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -10,5 +10,6 @@ def load_from_json_file(filename):
     """function that creates an
     Object from a “JSON file”:
     """
-    with open(filename, encoding="utf-8") as f:
-        json.load(f)
+    with open(filename, encoding="utf-8") as json_file:
+        json_data = json_file.read()
+        return json.loads(json_data)
