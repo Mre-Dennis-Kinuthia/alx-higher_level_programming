7cced02398075680662f2000e090dfab13551817
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-01-10 17:14:06 +0000

    init

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""_summary_
+"""Defines a class Student
 """
 
 
@@ -8,14 +8,22 @@ class Student:
     """
 
     def __init__(self, first_name, last_name, age):
-        """initialization function
+        """Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """function module to convert attributes to json
+        """Get a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+        Args:
+            attrs (list): (Optional) The attributes to represent.
         """
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
