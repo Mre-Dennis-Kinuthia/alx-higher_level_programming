d8d502dd5e7c1516c01e6c31273e46d07662d2fd
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-01-10 17:11:48 +0000

    init

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -6,6 +6,7 @@
 class Student:
     """class module
     """
+
     def __init__(self, first_name, last_name, age):
         """initialization function
         """
@@ -13,11 +14,10 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-
-def to_json(self, attrs=None):
-    """function module to convert attributes to json
-    """
-    if (type(attrs) == list and
-            all(type(ele) == str for ele in attrs)):
-        return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+    def to_json(self, attrs=None):
+        """function module to convert attributes to json
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+            return self.__dict__
