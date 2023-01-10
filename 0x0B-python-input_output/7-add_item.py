01b5f794909c77c1c1f7880e21fb76c3107618cc
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-01-10 15:41:54 +0000

    init

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,7 +3,16 @@
 """
 
 import json
-from 5-save_to_json_file.py import save_to_json_file
-from 6-load_from_json_file.py import load_from_json_file
+import sys
+save_to = __import__('5-save_to_json_file').save_to_json_file
+load_from = __import__('6-load_from_json_file').load_from_json_file
 
 
+sys.argv = []
+sys.argv = load_from("add_item.json")
+
+n = len(sys.argv)
+
+for i in range(1,n):
+    sys.argv.append(i)
+save_to(sys.argv, "add_item.json")
