66ce9be061fac624445ca24ff76ab03dea5ce0e8
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-01-10 13:16:50 +0000

    init

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,6 +6,8 @@ and returns the number of characters written
 
 
 def write_file(filename="", text=""):
+    """Writes a text file (UTF8) and retuns its lenth
+    """
     with open(filename, "w", encoding="utf-8") as f:
         f.write(text)
         return len(text)
