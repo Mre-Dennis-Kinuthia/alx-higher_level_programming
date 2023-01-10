62d0f90f82445abb435a525f2c3d340129ceaf18
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-01-10 16:45:55 +0000

    init

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-"""_summary_
-"""
+''' a class Student that defines a student
+'''
 
 
 class Student:
     '''module class student
     '''
+
     def __init__(self, first_name, last_name, age):
+        '''method __init__
+        '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(snt):
-        """_summary_
-
-    Args:
-        obj (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
+    def to_json(self):
+        '''method to_json
+        '''
         return self.__dict__
