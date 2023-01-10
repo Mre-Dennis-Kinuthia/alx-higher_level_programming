e6b364cc66246b0b5134e83be61a862cea2a63e5
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-01-10 14:23:33 +0000

    init

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Program that writes an Object
+to a text file, using a JSON representation
+"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """function that writes an Object to a text file,
+    using a JSON representation
+    """
+    with open(filename, 'w') as f:
+        f.write(json.dumps(my_obj))
