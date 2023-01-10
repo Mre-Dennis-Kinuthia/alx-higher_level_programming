5185bcfa163916a876715917f9fcd571b9af07b9
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-01-10 16:44:32 +0000

    init

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,6 +4,8 @@
 
 
 class Student:
+    '''module class student
+    '''
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
