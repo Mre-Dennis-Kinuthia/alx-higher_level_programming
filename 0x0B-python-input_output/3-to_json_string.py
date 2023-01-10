c55f7817240cde3032cddda51fc8c0d5f1ecc54e
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-01-10 14:01:27 +0000

    init

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+"""
+function that returns the JSON representation of an object
+"""
+
+
+def to_json_string(my_obj):
+    """
+    function that returns the JSON representation of an object (string):
+    """
+    return json.dumps(myobj)
