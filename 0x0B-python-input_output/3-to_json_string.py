554033ab0828bf2f9c12ae14b96ca430b80d6aba
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-01-10 14:05:14 +0000

    init

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import json
 """
 function that returns the JSON representation of an object
 """
+
+import json
 
 
 def to_json_string(my_obj):
     """
     function that returns the JSON representation of an object (string):
     """
-    return json.dumps(myobj)
+    return json.dumps(my_obj)
