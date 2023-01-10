020981a48d9ab4ecc715087a2be72a5e50d02155
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-01-10 16:00:35 +0000

    init

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-"""function declaration
 """
-
+Declearing function
+"""
 import json
-import sys
-save_to = __import__('5-save_to_json_file').save_to_json_file
-load_from = __import__('6-load_from_json_file').load_from_json_file
+import os
+import importlib
+from sys import argv
 
 
-sys.argv = []
-sys.argv = load_from("add_item.json")
+def save_to_json_file(my_obj, filename):
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
 
-n = len(sys.argv)
 
-for i in range(1,n):
-    sys.argv.append(i)
-save_to(sys.argv, "add_item.json")
+def load_from_json_file(filename):
+    with open(filename) as f:
+        return json.load(f)
+
+
+if __name__ == "__main__":
+    args = []
+    filename = "add_item.json"
+    if os.path.exists(filename):
+        args = load_from_json_file(filename)
+    for elem in argv[1:]:
+        args.append(elem)
+    save_to_json_file(args, filename)
