490670138c83538eaa6b8e421edb6277184bbe87
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-01-10 14:16:29 +0000

    init

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that returns an object 
+"""
+import json
+
+
+def from_json_string(my_str):
+    """function that returns an object
+    (Python data structure) represented by a JSON string
+    """
+    return json.loads(my_str)
