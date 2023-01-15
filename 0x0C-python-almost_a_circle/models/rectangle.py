42729f534c175791904cdb3fb5cdc6a8acb84be8
Mre-Dennis-Kinuthia <denniskinuthia1105@gmail.com>
2023-01-15 15:22:29 +0300

    initial commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-"""Decaring class"""
 from models.base import Base
 
-
 class Rectangle(Base):
-    """ Class representing a rectangle"""
-
-
+    """
+    Class Rectangle inherits from Base
+    Private instance attributes, each with its own public getter and setter:
+    __width -> width
+    __height -> height
+    __x -> x
+    __y -> y
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Class constructor: def __init__(self, width, height, x=0, y=0, id=None):
+        Call the super class with id - this super call with use the logic of the __init__ of the Base class
+        Assign each argument width, height, x and y to the right attribute
+        """
         super().__init__(id)
         self.__width = width
         self.__height = height
@@ -16,28 +24,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """Getter for width"""
         return self.__width
 
+    @width.setter
     def width(self, value):
+        """Setter for width"""
         self.__width = value
 
     @property
     def height(self):
+        """Getter for height"""
         return self.__height
 
+    @height.setter
     def height(self, value):
+        """Setter for height"""
         self.__height = value
 
     @property
     def x(self):
+        """Getter for x"""
         return self.__x
 
+    @x.setter
     def x(self, value):
+        """Setter for x"""
         self.__x = value
 
     @property
     def y(self):
+        """Getter for y"""
         return self.__y
 
+    @y.setter
     def y(self, value):
+        """Setter for y"""
         self.__y = value
