88dec5de74070b5234d3bb609c9cf82e601e018d
Mre-Dennis-Kinuthia <Denniskinuthia1105@gmail.com>
2023-03-31 23:02:10 +0300

    init

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    const reverseList = [];
-    for (let i = list.length - 1; i >= 0; i--) {
-        reverseList.push(list[i]);
-    }
-    return reverseList;
+  const reverseList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reverseList.push(list[i]);
+  }
+  return reverseList;
 };
