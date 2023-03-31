fcdcad69b786ed76ab3b1513ff5c878808d7f8e4
Mre-Dennis-Kinuthia <denniskinuthia1105@gmail.com>
2023-03-31 23:49:15 +0300

    init

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-"""A script that takes in a URL,
-Sends a request to the URL,
-And displays the value of the X-Request-Id variable found in the header ofthe response.
 """
-
-import sys
-import urllib.request
+script takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of the response
+"""
+from urllib.request import urlopen
+from sys import argv
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as resp:
-        print(dict(resp.headers).get("X-Request-Id"))
+    with urlopen(argv[1]) as response:
+        header = response.getheader('X-Request-Id')
+        print(header)
--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
-Python script that takes your Github credentials (username and password)
-and uses the Github API to display your id
+script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
 """
 import requests
-from requests.auth import HTTPBasicAuth
 from sys import argv
 
-if __name__ == '__main__':
-    url = 'https://api.github.com/users/{}'.format(argv[1])
-    r = requests.get(url,
-                     auth=HTTPBasicAuth(argv[1], argv[2]))
-    print(r.json().get('id'))
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    headers = {"Accept": "application/vnd.github+json"}
+    r = requests.get(url, headers=headers, auth=(argv[1], argv[2]))
+    user_id = r.json().get('id')
+    print(user_id)
