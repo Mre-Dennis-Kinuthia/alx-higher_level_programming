a79be5bf0d2336358482c06429ed78337e66650b
Mre-Dennis-Kinuthia <denniskinuthia1105@gmail.com>
2023-03-31 23:10:58 +0300

    init

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,8 +1,17 @@
 #!/usr/bin/node
-// script that concats 2 files.
-
-const args = process.argv.slice(2);
 const fs = require('fs');
-const first = fs.readFileSync('./' + args[0]);
-const second = fs.readFileSync('./' + args[1]);
-fs.writeFileSync('./' + args[2], first + second);
+
+// Get the file paths from command line arguments
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const destFile = process.argv[4];
+
+// Read the content of the source files
+const contentA = fs.readFileSync(fileA);
+const contentB = fs.readFileSync(fileB);
+
+// Concatenate the content of the files
+const contentC = Buffer.concat([contentA, contentB]);
+
+// Write the concatenated content to the destination file
+fs.writeFileSync(destFile, contentC);
