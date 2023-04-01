342fd5655c203d13ae91246fffc6286d55eec3e6
Mre-Dennis-Kinuthia <Denniskinuthia1105@gmail.com>
2023-04-01 17:58:10 +0300

    init

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""
-Script that fetches https://intranet.hbtn.io/status
-"""
+""" script that fetches https://alx-intranet.hbtn.io/status """
 import requests
 
-if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    text = r.text
-    print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+
+if __name__ == "__main__":
+    req = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:\n\t- type: {}\n\t- \
+content: {}".format(type(req.text), req.text))
