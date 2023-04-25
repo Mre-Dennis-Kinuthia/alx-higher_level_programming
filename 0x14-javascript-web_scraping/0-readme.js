4f1e14669fda2e64d50b9adcc27d6538af0376a2
Mre-Dennis-kinuthia <denniskinuthia1105@gmail.com>
2023-04-25 15:19:02 +0300

    0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(data);
+    }
+
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,1 @@
+# readme code on javascript web scraping
