f01c5a3f9399994203c67286be620f8fc61ed531
Mre-Dennis-kinuthia <denniskinuthia1105@gmail.com>
2023-04-25 20:33:26 +0300

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+fs.writeFile(filePath, stringToWrite, { encoding: 'utf-8' }, function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(`The string "${stringToWrite}" was written to the file ${filePath}!`);
+});
