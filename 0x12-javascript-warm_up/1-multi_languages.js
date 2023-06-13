717883b94a9b6b365636344ddfa3a01cd8e5c4d0
Mre-Dennis-Kinuthia <denniskinuthia1105@gmail.com>
2023-06-13 14:13:17 +0300

    init

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
-const myVar = 'C is fun\nPython is cool\nJavascript is amazing';
-console.log(myVar);
+
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
